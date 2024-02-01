b4b2df257bebc30cf8650ef1834cfe1c3dc29c11
Emre Yilmaz <dev.yilmazemre@gmail.com>
2024-02-01 18:31:30 +0300

    Add component to page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 // import Image from 'next/image'
 // import styles from './page.module.css'
 
-import Ios from '@/components/ios'
+// import Ios from '@/components/ios'
+import ManifestForm from '@/components/manifestForm'
 
 export default function Home() {
   return (
     <div>
-      <Ios />
+      <ManifestForm />
     </div>
   )
 }
