10443757fbf4dc8cb96bec821b63fbbd37ec0fe4
Emre Yilmaz <dev.yilmazemre@gmail.com>
2024-02-10 03:33:35 +0300

    Fix style manifestForm component

--- src/components/manifestForm/index.tsx
+++ src/components/manifestForm/index.tsx
@@ -4,16 +4,17 @@ import { InputText } from 'primereact/inputtext'
 import styles from './styles.module.css'
 import { ColorPicker } from 'primereact/colorpicker'
 import { Dropdown } from 'primereact/dropdown'
+import UploadImage from '../uploadImage'
 
 export default function ManifestForm() {
   const [name, setName] = useState('')
   const [shortName, setShortName] = useState('')
   const [description, setDescription] = useState('')
-  const [startUrl, setStartUrl] = useState('/')
-  const [display, setDisplay] = useState()
-  const [backgroundColor, setBackgroundColor] = useState('#')
-  const [themeColor, setThemeColor] = useState('#')
-  const [orientation, setOrientation] = useState()
+  const [startUrl, setStartUrl] = useState('')
+  const [display, setDisplay] = useState('')
+  const [orientation, setOrientation] = useState('')
+  const [themeColor, setThemeColor] = useState('')
+  const [backgroundColor, setBackgroundColor] = useState('')
 
   const displayList = [
     { name: 'fullscreen', value: 'fullscreen' },
@@ -31,11 +32,11 @@ export default function ManifestForm() {
     name: name,
     short_name: shortName,
     description: description,
-    start_url: startUrl,
+    start_url: `/${startUrl}`,
     display: display,
-    background_color: backgroundColor,
-    theme_color: themeColor,
     orientation: orientation,
+    theme_color: themeColor,
+    background_color: backgroundColor,
     icons: [
       {
         src: 'icon.png',
@@ -49,13 +50,12 @@ export default function ManifestForm() {
 
   return (
     <div>
-      <pre> {manifestJson}</pre>
-
-      <div className={styles.form}>
+      <div className={`${styles.areaContainer} ${styles.form}`}>
         <div className="p-float-label">
           <InputText
             value={name}
             onChange={(e) => setName(e.target.value)}
+            style={{ width: '100% ' }}
             tooltip="The name of the web app."
             tooltipOptions={{
               position: 'right',
@@ -69,6 +69,7 @@ export default function ManifestForm() {
           <InputText
             value={shortName}
             onChange={(e) => setShortName(e.target.value)}
+            style={{ width: '100% ' }}
             tooltip="A shorter version of the app name, which is used on the home screen or app launcher when space is limited."
             tooltipOptions={{
               position: 'right',
@@ -82,6 +83,7 @@ export default function ManifestForm() {
           <InputText
             value={description}
             onChange={(e) => setDescription(e.target.value)}
+            style={{ width: '100% ' }}
             tooltip="A brief description of the web app."
             tooltipOptions={{
               position: 'right',
@@ -96,6 +98,7 @@ export default function ManifestForm() {
             value={startUrl}
             onChange={(e) => setStartUrl(e.target.value)}
             tooltip="The URL that should be loaded when the app is launched."
+            style={{ width: '100% ' }}
             tooltipOptions={{
               position: 'right',
               showDelay: 350,
@@ -108,10 +111,11 @@ export default function ManifestForm() {
           <Dropdown
             value={display}
             onChange={(e) => setDisplay(e.value)}
+            style={{ width: '100% ' }}
             options={displayList}
             optionLabel="Display"
             showClear
-            placeholder="Select a Display Type"
+            placeholder="Select a Display"
             className="w-full md:w-14rem"
             tooltip="Specifies how the web app should be displayed. Possible values include 'fullscreen', 'standalone', 'minimal-ui', and 'browser'."
             tooltipOptions={{
@@ -121,51 +125,15 @@ export default function ManifestForm() {
             }}
           />
         </div>
-        <div className="p-float-label">
-          <InputText
-            value={backgroundColor}
-            onChange={(e) => setBackgroundColor(e.target.value)}
-            tooltip="The background color of the splash screen when the app is launched."
-            tooltipOptions={{
-              position: 'right',
-              showDelay: 350,
-              style: { width: '20em' },
-            }}
-          />
-          <label htmlFor="backgroundColor">Background Color</label>
-          <ColorPicker
-            format="hex"
-            value={backgroundColor}
-            onChange={(e) => setBackgroundColor(`#${e.value}`)}
-          />
-        </div>
-
-        <div className="p-float-label">
-          <InputText
-            value={themeColor}
-            onChange={(e) => setThemeColor(e.target.value)}
-            tooltip="The color used for the toolbar or address bar."
-            tooltipOptions={{
-              position: 'right',
-              showDelay: 350,
-              style: { width: '20em' },
-            }}
-          />
-          <label htmlFor="themeColor">Theme Color</label>
-          <ColorPicker
-            format="hex"
-            value={themeColor}
-            onChange={(e) => setThemeColor(`#${e.value}`)}
-          />
-        </div>
         <div>
           <Dropdown
             value={orientation}
             onChange={(e) => setOrientation(e.value)}
+            style={{ width: '100% ' }}
             options={orientationList}
             optionLabel="Orientation"
             showClear
-            placeholder="Select a Orientation Type"
+            placeholder="Select a Orientation"
             className="w-full md:w-1rem"
             tooltip="Specifies the default orientation of the web app (e.g., 'portrait' or 'landscape')."
             tooltipOptions={{
@@ -175,6 +143,54 @@ export default function ManifestForm() {
             }}
           />
         </div>
+        <div className={styles.inputWithColor}>
+          <div className="p-float-label">
+            <InputText
+              value={backgroundColor}
+              onChange={(e) => setBackgroundColor(e.target.value)}
+              style={{ width: '200%' }}
+              tooltip="The background color of the splash screen when the app is launched."
+              tooltipOptions={{
+                position: 'right',
+                showDelay: 350,
+                style: { width: '20em' },
+              }}
+            />
+            <label htmlFor="backgroundColor">Background Color</label>
+          </div>
+          <ColorPicker
+            className={styles.colorPicker}
+            format="hex"
+            value={backgroundColor}
+            onChange={(e) => setBackgroundColor(`#${e.value}`)}
+          />
+        </div>
+        <div className={styles.inputWithColor}>
+          <div className="p-float-label">
+            <InputText
+              value={themeColor}
+              onChange={(e) => setThemeColor(e.target.value)}
+              style={{ width: '200%' }}
+              tooltip="The color used for the toolbar or address bar."
+              tooltipOptions={{
+                position: 'right',
+                showDelay: 350,
+                style: { width: '20em' },
+              }}
+            />
+            <label htmlFor="themeColor">Theme Color</label>
+          </div>
+          <ColorPicker
+            className={styles.colorPicker}
+            format="hex"
+            value={themeColor}
+            onChange={(e) => setThemeColor(`#${e.value}`)}
+          />
+        </div>
+        <UploadImage />
+      </div>
+      <div className={styles.areaContainer}>
+        <pre className={styles.code}>{manifestJson}</pre>
       </div>
     </div>
   )
